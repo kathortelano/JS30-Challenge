42584a02a93a1d8ec78960113eab1f88eae1e563
Katrina Hortelano <k.r.hortelano96@gmail.com>
2020-10-27 17:15:51 -0700

    day 9 complete

--- client/components/index.js
+++ client/components/index.js
@@ -4,5 +4,14 @@ import { ArrayCardio1 } from "./ArrayCardio1";
 import { ArrCardio2 } from "./ArrCardio2";
 import { CSSVar } from "./CssVars";
 import { FlexGallery } from "./FlexGallery";
+import { DevTools } from "./DevTools";
 
-export { Ajax, Canvas, ArrayCardio1, ArrCardio2, CSSVar, FlexGallery };
+export {
+  Ajax,
+  Canvas,
+  ArrayCardio1,
+  ArrCardio2,
+  CSSVar,
+  FlexGallery,
+  DevTools,
+};
