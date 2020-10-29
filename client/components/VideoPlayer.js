f9737bc18e4e69b0cc017442b01458a2a737c7d5
Katrina Hortelano <k.r.hortelano96@gmail.com>
2020-10-28 18:14:13 -0700

    day 11 completed: converted tutorial lessons from plain js to react js

--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -1,7 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 import "../styles/VideoPlayer.css";
 import { FaPlay, FaPause } from "react-icons/fa";
+<<<<<<< HEAD
 import { RiFullscreenFill } from "react-icons/ri";
+=======
+>>>>>>> day 11 completed: converted tutorial lessons from plain js to react js
 //get elements
 //build functions
 //hook event listenrs
@@ -12,7 +15,10 @@ export const VideoPlayer = () => {
   const [scrubProgress, setScrubProgress] = useState();
   const [playbackRate, setPlaybackRate] = useState(1);
   const [volume, setVolume] = useState(1);
+<<<<<<< HEAD
 
+=======
+>>>>>>> day 11 completed: converted tutorial lessons from plain js to react js
   const [progress, setProgress] = useState(0);
   const videoRef = useRef(null);
   const progressRef = useRef(null);
@@ -52,6 +58,7 @@ export const VideoPlayer = () => {
     video.currentTime = scrubTime;
   }
 
+<<<<<<< HEAD
   function handleFullScreen() {
     if (!document.fullscreenElement) {
       video.requestFullscreen();
@@ -61,6 +68,11 @@ export const VideoPlayer = () => {
     setVideo(videoRef.current);
     setScrubProgress(progressRef.current);
     console.log(videoRef);
+=======
+  useEffect(() => {
+    setVideo(videoRef.current);
+    setScrubProgress(progressRef.current);
+>>>>>>> day 11 completed: converted tutorial lessons from plain js to react js
   });
 
   return (
@@ -70,7 +82,11 @@ export const VideoPlayer = () => {
           ref={videoRef}
           onClick={togglePlay}
           onTimeUpdate={handleProgress}
+<<<<<<< HEAD
           className={`player__video viewer `}
+=======
+          className="player__video viewer"
+>>>>>>> day 11 completed: converted tutorial lessons from plain js to react js
           src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
         ></video>
 
@@ -126,9 +142,12 @@ export const VideoPlayer = () => {
           >
             25s »
           </button>
+<<<<<<< HEAD
           <button className="player__button" onClick={handleFullScreen}>
             <RiFullscreenFill />
           </button>
+=======
+>>>>>>> day 11 completed: converted tutorial lessons from plain js to react js
         </div>
       </div>
     </div>
